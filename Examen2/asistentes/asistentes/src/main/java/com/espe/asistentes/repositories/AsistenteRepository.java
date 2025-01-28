c62769223201452d0b1420312f2988b5abf3857d
MateoRoman <105375095+MateoRoman@users.noreply.github.com>
2025-01-27 20:03:03 -0500

    Delete Examen2 directory

--- Examen2/asistentes/asistentes/src/main/java/com/espe/asistentes/repositories/AsistenteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.espe.asistentes.repositories;
-
-import com.espe.asistentes.model.entities.Asistente;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AsistenteRepository extends CrudRepository<Asistente, Long> {
-
-}
--- Examen2/eventos/src/main/java/com/espe/eventos/repositories/EventoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.espe.eventos.repositories;
-
-import com.espe.eventos.model.entities.Evento;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EventoRepository extends CrudRepository<Evento, Long> {
-}
